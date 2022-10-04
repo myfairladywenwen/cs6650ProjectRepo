710d267a7737d63e724faa3118f46e7989007e73
Jiangjiang Li <jiangjiangli@Jiangjiangs-MacBook-Air.local>
2022-10-03 18:27:51 -0700

    code logic works both locally and on EC2, but EC2 can't deal with 200k due to based in east region, need launch new EC2 to test

--- client/src/main/java/Poster.java
+++ client/src/main/java/Poster.java
@@ -3,16 +3,29 @@ import io.swagger.client.ApiException;
 import io.swagger.client.ApiResponse;
 import io.swagger.client.api.SkiersApi;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Poster implements Runnable{
     private LinkedBlockingQueue<MyLiftRide> eventQueue;
+    private CountDownLatch firstCountDown;
+    private CountDownLatch endCountDown;
     private Barrier successCounter;
     private Barrier failCounter;
-    public static String basePathLocal = "http://localhost:8080/server_war_exploded/skiers/12";
-    public static String basePathEC2 = "http://35.171.26.226:8080/server_war/skiers/12";
-    public Poster(LinkedBlockingQueue<MyLiftRide> eventQueue, Barrier successCounter, Barrier failCounter){
+    public static String basePathLocal = "http://localhost:8080/newServer_war_exploded/skiers/12";
+    //    public static String basePathEC2 = "http://100.24.33.130:8080/newServer_war/skiers/12";
+    public Poster(LinkedBlockingQueue<MyLiftRide> eventQueue, CountDownLatch firstCountDown, CountDownLatch endCountDown,
+                  Barrier successCounter, Barrier failCounter){
         this.eventQueue = eventQueue;
+        this.firstCountDown = firstCountDown;
+        this.endCountDown = endCountDown;
+        this.successCounter = successCounter;
+        this.failCounter = failCounter;
+    }
+    public Poster(LinkedBlockingQueue<MyLiftRide> eventQueue, CountDownLatch endCountDown,
+                  Barrier successCounter, Barrier failCounter){
+        this.eventQueue = eventQueue;
+        this.endCountDown = endCountDown;
         this.successCounter = successCounter;
         this.failCounter = failCounter;
     }
@@ -23,10 +36,6 @@ public class Poster implements Runnable{
     }
 
     private void sendPostRequest(){
-        if(successCounter.getVal() >= 200000){
-            System.out.println("done. Total success: " + successCounter.getVal());
-            return;
-        }
         ApiClient apiClient = new ApiClient();
         apiClient.setBasePath(basePathLocal);
 //        apiClient.setBasePath(basePathEC2);
@@ -35,12 +44,44 @@ public class Poster implements Runnable{
             MyLiftRide curr = eventQueue.poll();
             try {
                 ApiResponse<Void> res = skiersApi.writeNewLiftRideWithHttpInfo(curr.getBody(), curr.getResortID(), curr.getSeasonID(), curr.getDayID(),curr.getSkierID());
+//                System.out.println(res.getStatusCode());
                 int j = 0;
                 while(j < 5 && res.getStatusCode() != 201){
                     res = skiersApi.writeNewLiftRideWithHttpInfo(curr.getBody(), curr.getResortID(), curr.getSeasonID(), curr.getDayID(),curr.getSkierID());
                     j++;
                 }
                 if(res.getStatusCode() == 201){
+                    if(firstCountDown != null) {
+                        firstCountDown.countDown();
+                    }
+                    endCountDown.countDown();
+                    successCounter.inc();
+                }else{
+                    failCounter.inc();
+                }
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void sendPostRequestSingleThread(){
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(basePathLocal);
+//        apiClient.setBasePath(basePathEC2);
+        SkiersApi skiersApi = new SkiersApi(apiClient);
+        for(int i = 0; i < 10000; i++){
+            MyLiftRide curr = eventQueue.poll();
+            try {
+                ApiResponse<Void> res = skiersApi.writeNewLiftRideWithHttpInfo(curr.getBody(), curr.getResortID(), curr.getSeasonID(), curr.getDayID(),curr.getSkierID());
+//                System.out.println(res.getStatusCode());
+                int j = 0;
+                while(j < 5 && res.getStatusCode() != 201){
+                    res = skiersApi.writeNewLiftRideWithHttpInfo(curr.getBody(), curr.getResortID(), curr.getSeasonID(), curr.getDayID(),curr.getSkierID());
+                    j++;
+                }
+                if(res.getStatusCode() == 201){
+                    endCountDown.countDown();
                     successCounter.inc();
                 }else{
                     failCounter.inc();
--- client/src/main/java/SingleThreadClient.java
+++ client/src/main/java/SingleThreadClient.java
@@ -1,8 +1,8 @@
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class MultiThreadClient {
-    private static final int TOTAL_REQUEST = 200000;
-    private static final int MAX_THREADS = 168;
+public class SingleThreadClient {
+    private static final int TOTAL_REQUEST = 10000;
     public static void main(String[] arg) throws Exception {
         LinkedBlockingQueue<MyLiftRide> eventQueue = new LinkedBlockingQueue<>(TOTAL_REQUEST);
         Generator generator = new Generator(eventQueue);
@@ -12,31 +12,17 @@ public class MultiThreadClient {
         Thread thread = new Thread(generator);
         thread.start();
 
-        ExecutorService producerPool = Executors.newFixedThreadPool(MAX_THREADS);
-        CountDownLatch firstCountDown = new CountDownLatch(1000);
         CountDownLatch endCountDown = new CountDownLatch(TOTAL_REQUEST);
-        for (int i = 0; i < 32; i++)
-        {
-            Poster poster = new Poster(eventQueue, firstCountDown, endCountDown, successCounter, failCounter) ;
-            producerPool.execute(poster);
-        }
-        firstCountDown.await();
-
-        for(int i = 0; i < MAX_THREADS; i++){
-            Poster poster = new Poster(eventQueue, firstCountDown, endCountDown, successCounter, failCounter) ;
-            producerPool.execute(poster);
-        }
+        Poster poster = new Poster(eventQueue, endCountDown, successCounter, failCounter);
+        poster.sendPostRequestSingleThread();
 
         endCountDown.await();
-        producerPool.shutdown();
-        producerPool.awaitTermination(10, TimeUnit.SECONDS);
-
         long end = System.currentTimeMillis();
         long time = end-start;
-        System.out.println("max threads count: " + MAX_THREADS);
         System.out.println("total success: " + successCounter.getVal());
         System.out.println("total failure: " + failCounter.getVal());
         System.out.println("total time used in millisecond: " + time);
         System.out.println("total throughput in requests per second: " + (double)TOTAL_REQUEST/time * 1000);
     }
 }
+
