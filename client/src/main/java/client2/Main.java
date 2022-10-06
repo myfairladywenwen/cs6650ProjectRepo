1e5d25374dc0531f9148ac911471e34f43678f11
Wen <myfairlady0903@gmail.com>
2022-10-05 17:15:38 -0700

    update basePath in response to new url validation rule

--- client/src/main/java/client2/Main.java
+++ client/src/main/java/client2/Main.java
@@ -2,10 +2,10 @@ package client2;
 
 public class Main {
     //local = "http://localhost:8080/newServer_war_exploded/skiers/12";
-    //EC2: "http://34.220.181.128:8080/newServer_war/skiers/12";
+    //EC2: "http://34.220.181.128:8080/newServer_war/skiers/12/seasons/2019/days/365/skiers/123";
     private static final int TOTAL_REQUEST = 200000;
     private static final int MAX_THREADS = 168;
-    private static String basePath = "http://localhost:8080/newServer_war_exploded/skiers/12";
+    private static String basePath = "http://localhost:8080/newServer_war_exploded/skiers/12/seasons/2019/days/365/skiers/123";
 
     public static void main(String[] args) throws Exception {
         MultiThreadClient2 multiThreadClient2 = new MultiThreadClient2(basePath, TOTAL_REQUEST, MAX_THREADS);
